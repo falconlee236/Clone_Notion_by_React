3d761128bac018c17db0c5725a8b8a8c33842c32
falconlee236 <falconlee236@naver.com>
2022-10-24 13:42:07 +0900

    set header layout

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -4,6 +4,7 @@ import {MdChatBubbleOutline} from 'react-icons/md';
 import {HiOutlineClock} from 'react-icons/hi';
 import {AiOutlineStar} from 'react-icons/ai';
 import {BsThreeDots} from 'react-icons/bs';
+import ReactTooltip from 'react-tooltip';
 
 const Headerstyle = styled.div`
     display: flex;
@@ -13,21 +14,66 @@ const Headerstyle = styled.div`
     height: 50px;
 `;
 
+const HeaderButtonstyle = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 5px 10px 5px;
+    height: 25px;
+    width: 25px;
+    
+    &:hover{
+        background: rgba(55, 53, 47, 0.08);
+        color: rgb(55, 53, 47);
+    }
+    //border: solid black 3px;
+`
+
+const Tmpdiv = styled.div`
+    display: flex;
+`
+
 
 function Header(){
+    function newDateformat(){
+        const today = new Date();
+        return `${today.getMonth()}월 ${today.getDate()}일 편집`;
+    }
     return (
         <Headerstyle>
             <div>
-                시작하기
+                <HeaderButtonstyle style={{width: '80px'}}>
+                    <span style={{fontSize: '15px'}}>시작하기</span>
+                </HeaderButtonstyle>
             </div>
-            <div>
-                공유
-                <MdChatBubbleOutline size={30}/>
-                <HiOutlineClock />
-                <AiOutlineStar />
-                <BsThreeDots />
-            </div>
+            <Tmpdiv>
+                <HeaderButtonstyle style={{width: '100px'}}>
+                    <span data-tip data-for='bottom' style={{fontSize: '12px'}} >
+                        {newDateformat()}
+                    </span>
+                    <ReactTooltip id='bottom' place='bottom' effect='solid'>
+                        이것은 툴팁입니다
+                    </ReactTooltip>
+                </HeaderButtonstyle>
+                <HeaderButtonstyle>
+                    <span style={{fontSize: '12px'}}>공유</span>
+                </HeaderButtonstyle>
+                <HeaderButtonstyle>
+                    <MdChatBubbleOutline size={20}/>
+                </HeaderButtonstyle>
+                <HeaderButtonstyle>
+                    <HiOutlineClock size={20}/>
+                </HeaderButtonstyle>
+                <HeaderButtonstyle>
+                    <AiOutlineStar size={20}/>
+                </HeaderButtonstyle>
+                <HeaderButtonstyle>
+                    <BsThreeDots size={20}/>
+                </HeaderButtonstyle>
+            </Tmpdiv>
         </Headerstyle>
+
+        
     )
 }
 
